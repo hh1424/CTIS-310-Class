ac97b2d88f3439c8feed6aa92d46a0f8b4bffdcc
hh1424 <111393342+hh1424@users.noreply.github.com>
2023-04-21 09:20:18 -0400

    Work for CTIS 310
    Project 1

--- passwordgenerator/src/main/java/edu/guilford/App.java
+++ passwordgenerator/src/main/java/edu/guilford/App.java
@@ -6,6 +6,17 @@ package edu.guilford;
  */
 public class App 
 {
+
+    // Attributes for the user - for logins or creation of account
+    private String firstName;
+    private String email;
+    private String username;
+    private String password;
+    private String passwordConfirm;
+    private String phoneNum;
+
+
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
