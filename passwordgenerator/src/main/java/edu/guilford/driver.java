c919257f1c6a13ed4eede2a4d248852520cf5f8f
hh1424 <111393342+hh1424@users.noreply.github.com>
2023-04-21 10:47:25 -0400

    Project 1
    Work for CTIS 310

---
+++ passwordgenerator/src/main/java/edu/guilford/driver.java
@@ -0,0 +1,17 @@
+package edu.guilford;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class driver 
+{
+
+    public static void main( String[] args )
+    {
+        //Instantiate a scanner to read the passwords
+        Scanner scan = new Scanner(System.in);
+    }
+}
--- passwordgenerator/src/main/java/edu/guilford/user.java
+++ passwordgenerator/src/main/java/edu/guilford/user.java
@@ -21,4 +21,53 @@ public class user {
         this.passwordConfirm = passwordConfirm;
         this.phoneNum = phoneNum;
     }
+
+    //Getters and Setters for each attribute
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
 }
