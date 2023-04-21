3f33c88c1a6be75eca6308896cf60eab2766e899
hh1424 <111393342+hh1424@users.noreply.github.com>
2023-04-21 10:47:41 -0400

    Sandbox
    Work for CTIS 310

--- sandbox/src/main/java/edu/guilford/Sandbox.java
+++ sandbox/src/main/java/edu/guilford/Sandbox.java
@@ -10,18 +10,50 @@ public class Sandbox
 {
     public static void main( String[] args )
     {
+
         // Ask the user for an integer
         System.out.println( "Please enter an integer: " );
         // Declare a scanner to read from the keyboard
         Scanner keyboard = new Scanner(System.in);
         // Read the integer from the user
         int number = keyboard.nextInt();
+
+        // Build a switch statement that does different choices for 1, 2, 3, 4, and 5
+        switch (number) {
+            case 1:
+                System.out.println("You entered 1");
+                break;
+            case 2:
+                System.out.println("You entered 2");
+                break;
+            case 3:
+                System.out.println("You entered 3");
+                break;
+            case 4:
+                System.out.println("You entered 4");
+                break;
+            case 5:
+                System.out.println("You entered 5");
+                break;
+            default: // What's done if no other case is satisfied
+                System.out.println("You entered something else");
+                break;
+        }
         
         // Call the sum method and print the result
         System.out.println("The sum of the integers from 1 to " + number + " is " + sum(number));
 
         // Call the sumWhile method and print the result
         System.out.println("The sum of the integers from 1 to " + number + " is " + sumWhile(number));
+
+        //Print a message if the result of sumWhile is really big number
+        if (sumWhile(number) > 1000000) {
+            System.out.println("That's a big number!");
+        }
+        // Otherwise express sympathy that the number is so small
+        else {
+            System.out.println("That's a small number!");
+        }
 
     }
 
