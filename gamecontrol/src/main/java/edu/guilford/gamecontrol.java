7080b2ae34900625f6caef031ae01249c4e5d5bc
hh1424 <111393342+hh1424@users.noreply.github.com>
2023-04-21 10:47:04 -0400

    GameControl Example
    Work for CTIS 310

---
+++ gamecontrol/src/main/java/edu/guilford/gamecontrol.java
@@ -0,0 +1,46 @@
+package edu.guilford;
+
+/**
+ * Hello world!
+ *
+ */
+public class gamecontrol 
+{
+    public static void main( String[] args )
+    {
+
+    //Build a gamestate object
+    gamestate gameState = new gamestate();
+
+    //Initialize the game, then start it, wait 3 seconds, pause the game, wait 5 seconds, 
+    //then change to running, wait 4 more seconds, and end the game. print out the banner
+    //after each state change
+    gameState.startGame();
+    System.out.println(gameState.getBanner());
+    try {
+        Thread.sleep(3000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+    gameState.pauseGame();
+    System.out.println(gameState.getBanner());
+    try {
+        Thread.sleep(5000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+    gameState.resumeGame();
+    System.out.println(gameState.getBanner());
+    try {
+        Thread.sleep(4000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+    gameState.endGame();
+    System.out.println(gameState.getBanner());
+
+
+
+    }
+
+}
